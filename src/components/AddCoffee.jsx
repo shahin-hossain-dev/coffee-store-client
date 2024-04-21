0ab3b2e62ccca03e38cdc778ce692cb0d40bc4c6
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-04-21 21:04:44 +0600

    56-3 Server Side Hide Database User And Password Using Dotenv Config

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -3,7 +3,37 @@ import React from "react";
 const AddCoffee = () => {
   return (
     <div>
-      <h2>Add Coffee Page</h2>
+      <h2 className="text-xl">Add a Coffee </h2>
+      <form>
+        <div className="flex gap-12">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Coffee Name</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Coffee Name"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Available Quantity</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Available Quantity"
+              className="input input-bordered"
+              required
+            />
+          </div>
+        </div>
+
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Login</button>
+        </div>
+      </form>
     </div>
   );
 };
