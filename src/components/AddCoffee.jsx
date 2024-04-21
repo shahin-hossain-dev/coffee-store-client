6654500d5a742db89ec1e29542e182ed07a47b74
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-04-21 19:54:17 +0600

    56-2 Client Side Router And Connect MongoDB Database

---
+++ src/components/AddCoffee.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AddCoffee = () => {
+  return (
+    <div>
+      <h2>Add Coffee Page</h2>
+    </div>
+  );
+};
+
+export default AddCoffee;
+<h2>Add Coffee Page</h2>;
