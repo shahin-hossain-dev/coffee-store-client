b0058d8ca242fcf28808e1c659e4b5322f5e8d2e
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-04-23 00:31:23 +0600

    56_5-5 Delete A User From Client Side And Database

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,8 +1,23 @@
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const Users = () => {
   const loadedUsers = useLoaderData();
+  const [users, setUsers] = useState(loadedUsers);
   console.log(loadedUsers);
+  const handleDeleteUser = (id) => {
+    fetch(`http://localhost:5000/user/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.deletedCount > 0) {
+          alert("User Deleted");
+          const remaining = users.filter((user) => user._id !== id);
+          setUsers(remaining);
+        }
+      });
+  };
   return (
     <div className="w-1/2 mx-auto mt-5">
       <h2>Users page</h2>
@@ -19,12 +34,19 @@ const Users = () => {
           </thead>
           <tbody>
             {/* row 1 */}
-            {loadedUsers.map((user) => (
+            {users.map((user) => (
               <tr key={user._id}>
                 <th>1</th>
                 <td>{user.email}</td>
                 <td>{user.createdAt}</td>
-                <td>Action</td>
+                <td>
+                  <button
+                    onClick={() => handleDeleteUser(user._id)}
+                    className="btn"
+                  >
+                    Delete
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
